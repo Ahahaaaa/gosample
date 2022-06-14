58f4f89fcea0d30168030725abe5e9ad1809b4c7
Ahahaaaa <kwk88mmflak@gmail.com>
2022-06-14 16:02:28 +0900

    tem.go

--- template2/tem.go
+++ template2/tem.go
@@ -6,28 +6,22 @@ import (
     "net/http"
 )
  
-func gtplHandler(w http.ResponseWriter, r *http.Request) {
+func htmlHandler(w http.ResponseWriter, r *http.Request) {
  
-    // テンプレートをパースする
-    tpl := template.Must(template.ParseFiles("templates/sample.gtpl"))
+    tmp, _ := template.ParseFiles("tem.html")
  
-    // テンプレートに出力する値をマップにセット
     values := map[string]string{
         "account": "user-1",
         "name":    "さとうこうや",
         "passwd":  "sample-pass",
     }
  
-    // マップを展開してテンプレートを出力する
-    if err := tpl.ExecuteTemplate(w, "sample.gtpl", values); err != nil {
+    if err := tmp.Execute(w, values); err != nil {
         fmt.Println(err)
     }
 }
  
 func main() {
-    // ルートへのリクエストを"gtplHandler"関数で処理する
-    http.HandleFunc("/", gtplHandler)
- 
-    // localhost:8080でサーバー処理開始
+    http.HandleFunc("/", htmlHandler)
     http.ListenAndServe(":8080", nil)
 }
