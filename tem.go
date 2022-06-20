8ac7fe7fbad86bce4dafd13ada5eab629e36e4b8
Ahahaaaa <kwk88mmflak@gmail.com>
2022-06-20 07:27:27 +0000

    更新

--- tem.go
+++ tem.go
@@ -3,32 +3,34 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"os"
 )
- 
+
 func htmlHandler(w http.ResponseWriter, r *http.Request) {
- 
-    tmp, _ := template.ParseFiles("tem.html")
- 
-    values := map[string]string{
-        "account": "user-1",
-        "name":    "さとうこうや",
-        "passwd":  "sample-pass",
-    }
- 
-    if err := tmp.Execute(w, values); err != nil {
-        fmt.Println(err)
-    }
+
+	tmp, _ := template.ParseFiles("tem.html")
+
+	values := map[string]string{
+		"account": "user-1",
+		"name":    "さとうこうや",
+		"passwd":  "sample-pass",
+	}
+
+	if err := tmp.Execute(w, values); err != nil {
+		fmt.Println(err)
+	}
 }
- 
 
 func main() {
-    port:=os.Getenv("PORT")
-    if  port==""{
-    } else {
-        port = "8080"
-    }
-    http.HandleFunc("/", htmlHandler)
-    http.ListenAndServe(":"+port, nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.HandleFunc("/", htmlHandler)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
